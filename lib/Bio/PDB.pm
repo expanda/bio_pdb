049e341a92d83846ed05c15b8885157fb444d2ee
hiroyuki <hiroyuki@sfc.keio.ac.jp>
2009-01-09 16:48:27 +0900

    initial commit

---
+++ lib/Bio/PDB.pm
@@ -0,0 +1,33 @@
+package Bio::PDB;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Bio::PDB -
+
+=head1 SYNOPSIS
+
+  use Bio::PDB;
+
+=head1 DESCRIPTION
+
+Bio::PDB is
+
+=head1 AUTHOR
+
+hiroyuki nakamura E<lt>hiroyuki@sfc.keio.ac.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
